2421b0a8382703abf15a8161a7fb731fb7b4444e
GonzaloArigos <gonzaloarigos@gmail.com>
2018-08-16 15:37:21 -0300

    Primer commit solucion

---
+++ FunClub/SPA/Controller/AppController.js
@@ -0,0 +1,21 @@
+﻿var app = angular.module("app", ['ngRoute']);
+
+app.config(function ($routeProvider, $locationProvider) {
+    $routeProvider
+        .when('/',
+        {
+            templateUrl: 'SPA/Views/Home.html',
+            controller: 'HomeController'
+        })
+        .when('/Logoff',
+        {
+            templateUrl: 'Account/LogOff',
+            controller: 'HomeController'
+        })
+        .otherwise({
+            templateUrl: 'SPA/Views/Home.html',
+            controller: 'HomeController'
+        });
+
+
+});
