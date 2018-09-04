932152266a874d219007341b777ffdc74811ace6
GonzaloArigos <gonzaloarigos@gmail.com>
2018-09-04 20:23:35 -0300

    Front end de la parte de gestion de discotecas
    Front end

--- FunClub/SPA/Controller/DiscotecaController.js
+++ FunClub/SPA/Controller/DiscotecaController.js
@@ -1,8 +1,11 @@
-﻿angular.module('app').controller("PrincipalController", ["$scope", "PrincipalService", 'Excel', '$timeout', 'Upload', "$q", function ($scope, PrincipalService, Excel, $timeout,Upload, $q) {
+﻿angular.module('app').controller("DiscotecaController", ["$scope", "DiscotecaService", 'Excel', '$timeout', 'Upload', "$q", function ($scope, DiscotecaService, Excel, $timeout,Upload, $q) {
 
     $scope.Cargando = false;
     $scope.Ok = false;
     $scope.Resultado = [];
+
+    $scope.NuevaDisco = false;
+    $scope.GestionDisco = false;
 
     $scope.VerCorrectos = false;
     $scope.VerEx = false;
@@ -12,5 +15,7 @@
         $scope.exportHref = Excel.tableToExcel(tableId, 'sheet name');
         $timeout(function () { location.href = $scope.exportHref; }, 100); // trigger download
     }
+
+    
 
 }])
