4c16f4b3ea03b347f6a6c069af88b4d8badd544e
GonzaloArigos <gonzaloarigos@gmail.com>
2018-11-05 14:50:20 -0300

    agregado api para android

---
+++ FunClub/Controllers/PuntosController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FunClub.Controllers
+{
+    public class PuntosController : Controller
+    {
+        // GET: Puntos
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string AcreditarPuntosDNI(string dni,string disco)
+        {
+            return "Se creditaron 50 puntos al DNI";
+        }
+    }
+}
